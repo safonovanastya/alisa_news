ee43676d7d416524c3f51de75632dd8c2372a21e
safonovanastya <42999070+safonovanastya@users.noreply.github.com>
2020-12-14 16:37:17 +0300

    Add files via upload

--- api.py
+++ api.py
@@ -1,27 +1,104 @@
+# coding: utf-8
+# Импортирует поддержку UTF-8.
+from __future__ import unicode_literals
 
-from alice_scripts import Skill, request, say, suggest
-skill = Skill(__name__)
-FLASK_APP=hello.py flask run --with-threads
+# Импортируем модули для работы с JSON и логами.
+import json
+import logging
+
+# Импортируем подмодули Flask для запуска веб-сервиса.
+from flask import Flask, request
+app = Flask(__name__)
 
 
-@skill.script
-def run_script():
-yield say('Добрый день! Как вас зовут?')
-name = request.command
+logging.basicConfig(level=logging.DEBUG)
 
-yield say('Сколько вам лет?')
-while not request.matches(r'\d+'):
-yield say('Я вас не поняла. Скажите число')
-age = int(request.command)
+# Хранилище данных о сессиях.
+sessionStorage = {}
 
-yield say('Вы любите кошек или собак?',
-suggest('Обожаю кошечек', 'Люблю собак'))
-while not request.has_lemmas('кошка', 'кошечка',
-'собака', 'собачка'):
-yield say('У вас только два варианта - кошки или собаки')
-loves_cats = request.has_lemmas('кошка', 'кошечка')
+# Задаем параметры приложения Flask.
+@app.route("/", methods=['POST'])
 
-yield say(f'Рада познакомиться, {name}! Когда вам '
-f'исполнится {age + 1}, я могу подарить '
-f'{"котёнка" if loves_cats else "щенка"}!',
-end_session=True)
+def main():
+# Функция получает тело запроса и возвращает ответ.
+    logging.info('Request: %r', request.json)
+
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info('Response: %r', response)
+
+    return json.dumps(
+        response,
+        ensure_ascii=False,
+        indent=2
+    )
+
+# Функция для непосредственной обработки диалога.
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        # Это новый пользователь.
+        # Инициализируем сессию и поприветствуем его.
+
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+
+        res['response']['text'] = 'Привет! Купи слона!'
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+
+    # Обрабатываем ответ пользователя.
+    if req['request']['original_utterance'].lower() in [
+        'ладно',
+        'куплю',
+        'покупаю',
+        'хорошо',
+    ]:
+        # Пользователь согласился, прощаемся.
+        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
+        return
+
+    # Если нет, то убеждаем его купить слона!
+    res['response']['text'] = 'Все говорят "%s", а ты купи слона!' % (
+        req['request']['original_utterance']
+    )
+    res['response']['buttons'] = get_suggests(user_id)
+
+# Функция возвращает две подсказки для ответа.
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
+
+    # Выбираем две первые подсказки из массива.
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests'][:2]
+    ]
+
+    # Убираем первую подсказку, чтобы подсказки менялись каждый раз.
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
+
+    # Если осталась только одна подсказка, предлагаем подсказку
+    # со ссылкой на Яндекс.Маркет.
+    if len(suggests) < 2:
+        suggests.append({
+            "title": "Ладно",
+            "url": "https://market.yandex.ru/search?text=слон",
+            "hide": True
+        })
+
+    return suggests
