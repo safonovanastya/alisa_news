c1edd235b04c02b74531a915a18283802f838372
safonovanastya <42999070+safonovanastya@users.noreply.github.com>
2020-12-15 15:53:32 +0300

    Add files via upload

--- api.py
+++ api.py
@@ -93,11 +93,23 @@ def handle_dialog(req, res):
         link = response.json()['articles'][number]['url']
         image = response.json()['articles'][number]['urlToImage']
         ya_image_id = upload_yandex(image)
-        res['response']['text'] = 'Вот такая есть новость из категории ' + req['request']['original_utterance'].lower() + ': ' + title + '\n\n Хочешь ещё новость? Выбери категорию!'
-        res['response']['card']['type'] = 'BigImage' 
-        res['response']['card']['image_id'] = ya_image_id
-        res['response']['buttons'] = get_suggests(user_id)
 
+        res = {
+            "text": 'Вот такая есть новость из категории ' + req['request']['original_utterance'].lower() + ': ' + title + '\n\n Хочешь ещё новость? Выбери категорию!',
+            "card": {
+            "type": "BigImage",
+            "image_id": ya_image_id,
+            "button": {
+                "text": "Подробнее",
+                "url": link,
+                "payload": {}
+            }
+            },
+            "buttons": get_suggests(user_id),
+            "end_session": false
+        },
+        "version": "1.0"
+        }
         return
 
 
