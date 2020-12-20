f3a81b9ccbb27e1b5c6d21247a6f5a2714ab6d17
safonovanastya <42999070+safonovanastya@users.noreply.github.com>
2020-12-20 14:47:59 +0300

    Add files via upload

--- api.py
+++ api.py
@@ -42,149 +42,18 @@ def main():
         indent=2
     )
 
-import pprint
-import time
-from urlparse import urlparse
+def upload_yandex(img):
+    url = 'https://dialogs.yandex.net/api/v1/skills/c7ab78ae-4fb6-4ea8-bed3-239fa4c140d4/images'
 
-from requests import HTTPError
+    payload = {
+        "url": img 
+    }
+    headers = {"Authorization": "OAuth AgAAAAAFVw__AAT7o0bK8BXYR0elqUK5b9JzBUc",
+               "Content-Type": "application/json; charset=utf-8"}
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
 
-class YandexImages(object):
-    def __init__(self):
-        self.SESSION = requests.Session()
-        #self.SESSION.headers.update(AUTH_HEADER)
-
-        self.API_VERSION = 'v1'
-        self.API_BASE_URL = 'https://dialogs.yandex.net/api/'
-	self.API_URL = self.API_BASE_URL + '/' + self.API_VERSION + '/'
-	self.skills = ''
-
-    def set_auth_token(self, token):
-        self.SESSION.headers.update(self.get_auth_header(token))
-
-    def get_auth_header(self, token):
-        return {
-            'Authorization': 'OAuth %s' % token,
-            "Content-Type": "application/json; charset=utf-8"
-        }
-
-    def log(self, error_text,response):
-        log_file = open('YandexApi.log','a')
-        log_file.write(error_text+'\n')#+response)
-        log_file.close()
-
-    def validate_api_response(self, response, required_key_name=None):
-        content_type = response.headers['Content-Type']
-        content = json.loads(response.text) if 'application/json' in content_type else None
-
-        if response.status_code == 200:
-            if required_key_name and required_key_name not in content:
-                self.log('Unexpected API response. Missing required key: %s' % required_key_name, response=response)
-                return None
-        elif content and 'error_message' in content:
-            self.log('Error API response. Error message: %s' % content['error_message'], response=response)
-            return None
-        elif content and 'message' in content:
-            self.log('Error API response. Error message: %s' % content['message'], response=response)
-            return None
-        else:
-            response.raise_for_status()
-
-        return content
-
-    ################################################
-    # Проверить занятое место                      #
-    #                                              #
-    # Вернет массив                                #
-    # - total - Сколько всего места осталось       #
-    # - used - Занятое место                       #
-    ################################################
-    def checkOutPlace(self):
-        result = self.SESSION.get(self.API_URL+'status')
-        content = self.validate_api_response(result)
-        if content != None:
-            return content['images']['quota']
-        return None
-
-    ################################################
-    # Загрузка изображения из интернета            #
-    #                                              #
-    # Вернет массив                                #
-    # - id - Идентификатор изображения             #
-    # - origUrl - Адрес изображения.               #
-    ################################################
-    def downloadImageUrl(self,url):
-        path = 'skills/{skills_id}/images'.format(skills_id=self.skills)
-        result = self.SESSION.post(url=self.API_URL+path, data=json.dumps({"url":url}), headers=self.SESSION.headers)
-        content = self.validate_api_response(result)
-        if content != None:
-            return content['image']
-        return None
-
-
-    ################################################
-    # Загрузка изображения из файла                #
-    #                                              #
-    # Вернет массив                                #
-    # - id - Идентификатор изображения             #
-    # - origUrl - Адрес изображения.               #
-    ################################################
-    def downloadImageFile(self, img):
-        path = 'skills/{skills_id}/images'.format(skills_id=self.skills)
-        result = self.SESSION.post(url = self.API_URL+path,files={'file':(img,open(img,'rb'))})
-        content = self.validate_api_response(result)
-        if content != None:
-            return content['image']
-        return None
-
-    ################################################
-    # Просмотр всех загруженных изображений        #
-    #                                              #
-    # Вернет массив из изображений                 #
-    # - id - Идентификатор изображения             #
-    # - origUrl - Адрес изображения.	           #
-    ################################################
-    def getLoadedImages(self):
-        path = 'skills/{skills_id}/images'.format(skills_id=self.skills)
-        result = self.SESSION.get(url = self.API_URL+path)
-        content = self.validate_api_response(result)
-        if content != None:
-            return content['images']
-        return None
-
-    ################################################
-    # Удаление выбранной картинки                  #
-    #                                              #
-    # В случае успеха вернет 'ok'	               #
-    ################################################
-    def deleteImage(self, img_id):
-        path = 'skills/{skills_id}/images/{img_id}'.format(skills_id=self.skills,img_id = img_id)
-        result = self.SESSION.delete(url=self.API_URL+path)
-        content = self.validate_api_response(result)
-        if content != None:
-            return content['result']
-        return None
-
-    def deleteAllImage(self):
-        success = 0
-        fail = 0
-        images = self.getLoadedImages()
-        for image in images:
-            image_id = image['id']
-            if image_id:
-                if self.deleteImage(image_id):
-                    success+=1
-                else:
-                    fail += 1
-            else:
-                fail += 1
-
-        return {'success':success,'fail':fail}
-
-
-yandex = YandexImages()
-yandex.set_auth_token(token = 'AgAAAAAFVw__AAT7o0bK8BXYR0elqUK5b9JzBUc')
-yandex.skills = 'c7ab78ae-4fb6-4ea8-bed3-239fa4c140d4'
-
+    data = json.loads(r.content)
+    return data["image"]["id"]
 
 # Функция для непосредственной обработки диалога.
 def handle_dialog(req, res):
@@ -223,7 +92,7 @@ def handle_dialog(req, res):
         title = response.json()['articles'][number]['title']
         link = response.json()['articles'][number]['url']
         image = response.json()['articles'][number]['urlToImage']
-        ya_image_id = downloadImageUrl(image)
+        ya_image_id = upload_yandex(image)
 
         res['response']['text'] = 'Вот такая есть новость из категории ' + req['request']['original_utterance'].lower() + ':\n' + title + '\n\n Для подробной информации нажми на картинку. \n\n\n Хочешь ещё новость? Выбери категорию!'
         res['response']['card'] = {'type' : 'BigImage', 'image_id' : ya_image_id, 'button' : {'text' : 'Подробнее', 'url' : link}}
